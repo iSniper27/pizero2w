8eb97b97fcb6afab3d7b0afc098922af24083f4a
DBosPriv <bosdaan15@gmail.com>
2025-01-07 19:22:24 +0100

    gpiozero convert

--- test1.py
+++ test1.py
@@ -1,18 +1,13 @@
-import RPi.GPIO as io
+from gpiozero import LED
 import time
 from flask import Flask, json, request, render_template
 
 app = Flask(__name__)
-io.setmode(io.BCM)
 pins = {
-    22 : 'red',
-    23 : 'green',
-    24 : 'blue'
-   }
-
-for pin in pins:
-    io.setup(pin, io.OUT)
-    io.output(pin, io.LOW)
+    LED(22) : 'red',
+    LED(23) : 'green',
+    LED(24) : 'blue'
+}
 
 @app.route('/')
 def main():
@@ -23,9 +18,9 @@ def flash():
     colour = request.args.get('colour')
     if colour in pins.values():
         pin = [i for i in pins if pins[i]==colour]
-        io.output(pin, io.HIGH)
+        pin.on()
         time.sleep(1)
-        io.output(pin, io.LOW)
+        pin.off()
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     else:
         return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
