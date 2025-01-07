7a5649c9735cb94f0d3263484fb7b9ae41f9a02a
DBosPriv <bosdaan15@gmail.com>
2025-01-07 19:30:20 +0100

    remove debug

--- test1.py
+++ test1.py
@@ -26,4 +26,4 @@ def flash():
         return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
