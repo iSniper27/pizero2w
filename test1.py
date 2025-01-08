2d2e9dca411f46fb15d887b951aa672bc6ff7dd2
DBosPriv <bosdaan15@gmail.com>
2025-01-08 15:59:47 +0100

    rgb led

--- test1.py
+++ test1.py
@@ -1,13 +1,16 @@
-from gpiozero import LED
+from gpiozero import LED, RGBLED
 import time
 from flask import Flask, json, request, render_template
 
 app = Flask(__name__)
+
 pins = {
     'red' : LED(22),
     'green' : LED(23),
     'blue' : LED(24)
 }
+
+rgbled = RGBLED(red=9, green=10, blue=11, active_high=False)
 
 @app.route('/')
 def main():
@@ -23,6 +26,15 @@ def flash():
         pin.off()
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     else:
+        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+    
+@app.route('/rgb')
+def rgb():
+    colour = request.args.get('colour')
+    try:
+        rgbled.color = colour
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    except:
         return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
 
 if __name__ == "__main__":
