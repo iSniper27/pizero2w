fdbef2d5f76a466e863db8fc78f6d2e1c8276c66
DBosPriv <bosdaan15@gmail.com>
2025-01-17 14:28:51 +0100

    write route different

--- test1.py
+++ test1.py
@@ -7,32 +7,28 @@ app = Flask(__name__)
 pins = {
     'red' : LED(22),
     'green' : LED(23),
-    'blue' : LED(24)
+    'blue' : LED(24),
+    'rgb' : RGBLED(red=9, green=10, blue=11, active_high=False)
 }
-
-rgbled = RGBLED(red=9, green=10, blue=11, active_high=False)
 
 @app.route('/')
 def main():
     return render_template('main.html')
 
-@app.route('/flash')
+@app.route('/light')
 def flash():
-    colour = request.args.get('colour')
-    if colour in pins.keys():
-        pin = pins.get(colour)
-        pin.on()
-        time.sleep(1)
-        pin.off()
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-    else:
-        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
-    
-@app.route('/rgb')
-def rgb():
-    colour = eval(request.args.get('colour'))
+    type = request.args.get('type')
     try:
-        rgbled.color = colour
+        if type == 'led':
+            colour = request.args.get('colour')
+            pin = pins.get(colour)
+            pin.on()
+            time.sleep(1)
+            pin.off()
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+        elif type == 'rgb':
+            rgbcode = eval(request.args.get('rgbcode'))
+            pins['rgb'].color = rgbcode
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     except:
         return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
