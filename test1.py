6ee63d752c06d64f88131ae002b21d1c8bfbcc29
DBosPriv <bosdaan15@gmail.com>
2025-01-18 15:42:18 +0100

    flash buttons multiple

--- test1.py
+++ test1.py
@@ -4,16 +4,17 @@ from flask import Flask, json, request, render_template
 
 app = Flask(__name__)
 
-pins = {
+leds = {
     'red' : LED(22),
     'green' : LED(23),
-    'blue' : LED(24),
-    'rgb' : RGBLED(red=9, green=10, blue=11, active_high=False)
+    'blue' : LED(24)
 }
+
+rgb = RGBLED(red=9, green=10, blue=11, active_high=False)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('main.html', leds=leds)
 
 @app.route('/light')
 def flash():
@@ -21,14 +22,14 @@ def flash():
     try:
         if type == 'led':
             colour = request.args.get('colour')
-            pin = pins.get(colour)
+            pin = leds.get(colour)
             pin.on()
             time.sleep(1)
             pin.off()
             return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
         elif type == 'rgb':
             rgbcode = eval(request.args.get('rgbcode'))
-            pins['rgb'].color = rgbcode
+            rgb.color = rgbcode
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     except:
         return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
