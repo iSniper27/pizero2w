1889f1c8751300d2f64e1b34a40ac13bcbfe05e4
DBosPriv <bosdaan15@gmail.com>
2025-04-08 20:14:23 +0200

    threading

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 from gpiozero import LED, RGBLED, Buzzer
 import time
-from flask import Flask, request
+import threading
+from flask import Flask, json
 
 app = Flask(__name__)
 
@@ -13,12 +14,19 @@ leds = {
 rgb = RGBLED(red=9, green=10, blue=11, active_high=False)
 buzzer = Buzzer(26)
 
-@app.route('/flash/<color>')
-def flashred(color):
-    pin = leds.get(color)
+def flash_led(pin):
     pin.on()
     time.sleep(1)
     pin.off()
+
+@app.route('/flash/<color>')
+def flash_color(color):
+    pin = leds.get(color)
+    if pin:
+        threading.Thread(target=flash_led, args=(pin,)).start()
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    else:
+        return json.dumps({'success': False, 'error': 'Invalid color'}), 400, {'ContentType': 'application/json'}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
