5d1f4bbf8f2fed4b2c117274c157e8599233b491
iSniper27 <bosdaan11@gmail.com>
2025-04-11 21:34:06 +0200

    music v3

--- test2.py
+++ test2.py
@@ -1,4 +1,5 @@
 from time import sleep
+import music
 
 # Example song string from your comment
 song = "0 D4 8 0;0 D5 8 0;0 G4 8 0;8 C5 2 0;10 B4 2 0;12 G4 2 0;14 F4 1 0;15 G4 17 0;16 D4 8 0;24 C4 8 0"
