5cd1e08c9cfe7827de39f7e909d36446276b2bbd
iSniper27 <bosdaan11@gmail.com>
2025-04-11 21:17:28 +0200

    buzzer sleep

--- test2.py
+++ test2.py
@@ -1,9 +1,13 @@
 from gpiozero import TonalBuzzer
 from gpiozero.tones import Tone
+from time import sleep
 
 buzzer = TonalBuzzer(13)
 
 def playBuzzer(buzzer):
     buzzer.play(Tone("A4"))
+    sleep(1)
+    buzzer.stop()
+    
 
 playBuzzer(buzzer)
