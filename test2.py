ab7fd2227534bc22662508c84d8c0691fe8c5431
iSniper27 <bosdaan11@gmail.com>
2025-04-11 21:23:31 +0200

    more tones

--- test2.py
+++ test2.py
@@ -6,7 +6,17 @@ buzzer = TonalBuzzer(13)
 
 def playBuzzer(buzzer):
     buzzer.play(Tone("A4"))
-    sleep(1)
+    sleep(0.2)
+    buzzer.play(Tone("B4"))
+    sleep(0.2)
+    buzzer.play(Tone("C4"))
+    sleep(0.2)
+    buzzer.play(Tone("D4"))
+    sleep(0.2)
+    buzzer.play(Tone("E4"))
+    sleep(0.2)
+    buzzer.play(Tone("F4"))
+    sleep(0.2)
     buzzer.stop()
     
 
