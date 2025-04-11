9222a3193dc8b272a11e898bc501aabca1f51965
iSniper27 <bosdaan11@gmail.com>
2025-04-11 22:06:23 +0200

    pin change

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ from time import sleep
 from music import Music
 
 song = '0 D4 8 0;0 D5 8 0;0 G4 8 0;8 C5 2 0;10 B4 2 0;12 G4 2 0;14 F4 1 0;15 G4 17 0'
-music_player = Music(songString=song, tempo=3, pins=[17])
+music_player = Music(songString=song, tempo=3, pins=[13])
 
 try:
     while music_player.tick():
