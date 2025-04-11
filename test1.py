119a5796378896da7fe7900809a787fa0a6135b9
iSniper27 <bosdaan11@gmail.com>
2025-04-11 16:56:51 +0200

    no eventlet

--- test1.py
+++ test1.py
@@ -1,16 +1,14 @@
 from gpiozero import LED, RGBLED, Buzzer
 import time
+import threading
 from flask import Flask, json
 from flask_cors import CORS
-from flask_socketio import SocketIO
-
-import eventlet
-eventlet.monkey_patch()
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 CORS(app)
 
-socketio = SocketIO(app, cors_allowed_origins='*')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 leds = {
     'red': LED(22),
@@ -21,20 +19,21 @@ leds = {
 rgb = RGBLED(red=9, green=10, blue=11, active_high=False)
 buzzer = Buzzer(26)
 
-def flash_led(pin, color):
+def flash_led(pin):
     pin.on()
     time.sleep(1)
     pin.off()
-    socketio.emit('led_flashing', {'color': color})
+    socketio.emit('led_flashing', {'status': 'done'})
 
 @app.route('/flash/<color>')
 def flash_color(color):
     pin = leds.get(color)
     if pin:
-        eventlet.spawn(flash_led, pin, color)
+        threading.Thread(target=flash_led, args=(pin,)).start()
         return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
     else:
         return json.dumps({'success': False, 'error': 'Invalid color'}), 400, {'ContentType': 'application/json'}
 
+# Run the server
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', port=5000)
