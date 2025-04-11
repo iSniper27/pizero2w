7d52f8f17490154dba9cacee67f639f12a15e7f7
iSniper27 <bosdaan11@gmail.com>
2025-04-11 17:47:45 +0200

    socket io out of thread

--- test1.py
+++ test1.py
@@ -19,17 +19,17 @@ leds = {
 #rgb = RGBLED(red=9, green=10, blue=11, active_high=False)
 #buzzer = Buzzer(26)
 
-def flash_led(pin,color):
+def flash_led(pin):
     pin.on()
     time.sleep(1)
     pin.off()
-    socketio.emit('led_flashing', {'color': color})
 
 @app.route('/flash/<color>')
 def flash_color(color):
     pin = leds.get(color)
     if pin:
-        threading.Thread(target=flash_led, args=(pin,color)).start()
+        socketio.emit('led_flashing', {'color': color})
+        threading.Thread(target=flash_led, args=(pin)).start()
         return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
     else:
         return json.dumps({'success': False, 'error': 'Invalid color'}), 400, {'ContentType': 'application/json'}
