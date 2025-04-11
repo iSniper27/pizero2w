8477ea8f7e30a3d2d38d9d7f464a0285c9393d83
iSniper27 <bosdaan11@gmail.com>
2025-04-11 20:01:14 +0200

    empty string

--- test1.py
+++ test1.py
@@ -20,7 +20,7 @@ def flash_led(pin, color):
     pin.on()
     time.sleep(1)
     pin.off()
-    socketio.emit('led_flashing', {'color': 'black'})
+    socketio.emit('led_flashing', {'color': ''})
 
 @app.route('/flash/<color>')
 def flash_color(color):
