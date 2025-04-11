a4198bc8577e4d65609b017816087db23e3baa98
iSniper27 <bosdaan11@gmail.com>
2025-04-11 22:05:43 +0200

    try chatgpt

--- test2.py
+++ test2.py
@@ -1,17 +1,11 @@
-
-from gpiozero import Pin
-from music import music
 from time import sleep
+from music import Music
 
-f = open('words.txt', 'r')
-song = f.read()
+song = '0 D4 8 0;0 D5 8 0;0 G4 8 0;8 C5 2 0;10 B4 2 0;12 G4 2 0;14 F4 1 0;15 G4 17 0'
+music_player = Music(songString=song, tempo=3, pins=[17])
 
-#One buzzer on pin 0
-mySong = music(song, pins=[Pin(13)])
-
-#Four buzzers
-#mySong = music(song, pins=[Pin(0),Pin(1),Pin(2),Pin(3)])
-
-while True:
-    print(mySong.tick())
-    sleep(0.04)
+try:
+    while music_player.tick():
+        sleep(0.01)  # Adjust timing if needed for smoother playback
+except KeyboardInterrupt:
+    music_player.stop()
